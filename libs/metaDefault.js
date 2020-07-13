882b280a3f304801fde3354659af1881b0bf8e12
abusedmedia <ff@todo.to.it>
2020-07-13 19:26:28 +0200

    meta

--- libs/metaDefault.js
+++ libs/metaDefault.js
@@ -4,24 +4,24 @@ module.exports = [
   { name: 'robots', content: 'index, follow' },
 
   { property: 'og:type', content: 'website' },
-  { property: 'og:site_name', content: 'Presenta, a presentation tool' },
-  { property: 'og:description', content: 'Build presentations the modern way.' },
+  { property: 'og:site_name', content: 'Fabio Franchino' },
+  { property: 'og:description', content: 'Senior Designer / Technologist' },
 
-  { hid: 'ogtit', property: 'og:title', content: 'Presenta, a presentation tool' },
-  { hid: 'ogurl', property: 'og:url', content: 'https://www.presenta.cc/' },
-  { hid: 'ogimg', property: 'og:image', content: 'https://www.presenta.cc/social.png' },
+  { hid: 'ogtit', property: 'og:title', content: 'Fabio Franchino' },
+  { hid: 'ogurl', property: 'og:url', content: 'https://fabiofranchino.com/' },
+  { hid: 'ogimg', property: 'og:image', content: 'https://fabiofranchino.com/social.png' },
 
   { name: 'twitter:card', content: 'summary_large_image' },
   { name: 'twitter:site', content: '@fabiofranchino' },
   { name: 'twitter:creator', content: '@fabiofranchino' },
-  { name: 'twitter:description', content: 'Build presentations the modern way.' },
+  { name: 'twitter:description', content: 'Senior Designer / Technologist' },
 
   { hid: 'twtit', name: 'twitter:title', content: 'Presenta, a presentation tool' },
-  { hid: 'twurl', name: 'twitter:url', content: 'https://www.presenta.cc/' },
-  { hid: 'twimg', name: 'twitter:image', content: 'https://www.presenta.cc/social.png' },
+  { hid: 'twurl', name: 'twitter:url', content: 'https://fabiofranchino.com/' },
+  { hid: 'twimg', name: 'twitter:image', content: 'https://fabiofranchino.com/social.png' },
 
-  { hid: 'description', name: 'description', content: 'Presenta, a presentation tool. Build presentations the modern way.' },
-  { hid: 'keywords', name: 'keywords', content: 'presentation, data, information, presenting, speech, pitch' },
+  { hid: 'description', name: 'description', content: 'Senior Designer / Technologist' },
+  { hid: 'keywords', name: 'keywords', content: 'senior, designer, technologist' },
   { hid: 'author', name: 'author', content: 'Fabio Franchino' },
   { hid: 'published_time', name: 'article:published_time', content: '' }
 
