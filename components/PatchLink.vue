93dc697ce88894606bcd6fa6376e65b6d43f3a4a
abusedmedia <hello@fabiofranchino.com>
2021-01-22 11:51:08 +0100

    added tw cta

--- components/PatchLink.vue
+++ components/PatchLink.vue
@@ -2,6 +2,9 @@
     <div class="mini">
         Spotted a typo? 
         <a :href="editLink" target="_blank">Send a patch</a>
+        <br />
+        Want to comment? <strong>Reach me out on</strong>
+        <a href="https://twitter.com/fabiofranchino" target="_blank">Twitter</a>
     </div>
 </template>
 
