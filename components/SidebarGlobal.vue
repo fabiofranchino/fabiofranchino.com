69e7c25195d1c3072cd5426c306f4d5d9a27e5ff
abusedmedia <hello@fabiofranchino.com>
2021-10-04 07:38:18 +0200

    upd

--- components/SidebarGlobal.vue
+++ components/SidebarGlobal.vue
@@ -18,8 +18,8 @@
 
             <div>
 
-                <a href="https://app.presenta.cc/f/redir/?curl=zGywhb2oJn&purl=5zx5p8QaB">
-                    <img class="banner" src="https://app.presenta.cc/f/render/?curl=zGywhb2oJn&purl=5zx5p8QaB&w=200" />
+                <a href="https://app.presenta.cc/f/redir/zGywhb2oJn:5zx5p8QaB">
+                    <img class="banner" src="https://app.presenta.cc/f/render/zGywhb2oJn:5zx5p8QaB/?w=200" />
                 </a>
 
                 
