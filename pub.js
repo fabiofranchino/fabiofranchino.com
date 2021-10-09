f9b0c927fba381f56f8dfc183c3afefc90876678
abusedmedia <hello@fabiofranchino.com>
2021-10-09 21:48:55 +0200

    tags

--- pub.js
+++ pub.js
@@ -1,38 +1,38 @@
 const fs = require('fs')
-var slugify = require('slugify')
-var moment = require('moment')
-var path = require('path')
+const slugify = require('slugify')
+const moment = require('moment')
+const path = require('path')
 
-var logs = fs.readdirSync('./pub')
-var newfiles = logs.filter(f => {
+const logs = fs.readdirSync('./pub')
+const newfiles = logs.filter(f => {
   // no start with date and no start with .
   return !f.match(/\d\d\d\d-\d\d-\d\d/) && !f.match(/^[\.]/)
 })
 
 newfiles.forEach(f => {
-  var src = fs.readFileSync(`./pub/${f}`, 'utf8')
-  var rawtitle = f.split('.')[0]
-  var title = rawtitle
-  var filename = slugify(title).toLowerCase()
+  let src = fs.readFileSync(`./pub/${f}`, 'utf8')
+  const rawtitle = f.split('.')[0]
+  const title = rawtitle
+  const filename = slugify(title).toLowerCase()
 
   src = src.replace(title, '')
 
-  var re = /!\[(.*)\]\((.*)\)/gim
-  var mtc = src.match(re)
+  let re = /!\[(.*)\]\((.*)\)/gim
+  let mtc = src.match(re)
 
   if (mtc) {
     mtc.forEach(d => {
       re = /!\[(.*)\]\((.*)\)/gim
-      var rex = re.exec(d)
-      var r = parseInt(Math.random() * 1000)
-      var absPath = path.resolve('pub', rex[2])
+      const rex = re.exec(d)
+      const r = parseInt(Math.random() * 1000)
+      const absPath = path.resolve('pub', rex[2])
 
-      var assetName = slugify(`n${r}_` + path.basename(absPath)).toLowerCase()
+      const assetName = slugify(`n${r}_` + path.basename(absPath)).toLowerCase()
 
-      var newpath = `./static/assets/log/${assetName}`
+      const newpath = `./static/assets/log/${assetName}`
       fs.renameSync(absPath, newpath)
 
-      var newtag = `![](/assets/log/${assetName})`
+      const newtag = `![](/assets/log/${assetName})`
       src = src.replace(rex[0], newtag)
     })
   }
@@ -41,30 +41,30 @@ newfiles.forEach(f => {
   mtc = src.match(re)
   if (mtc) {
     mtc.forEach(d => {
-      var rex = re.exec(d)
+      const rex = re.exec(d)
 
-      var r = parseInt(Math.random() * 1000)
-      var absPath = path.resolve('pub', rex[1])
+      const r = parseInt(Math.random() * 1000)
+      const absPath = path.resolve('pub', rex[1])
 
-      var assetName = slugify(`n${r}_` + path.basename(absPath)).toLowerCase()
+      const assetName = slugify(`n${r}_` + path.basename(absPath)).toLowerCase()
 
-      var newpath = `./static/assets/log/${assetName}`
+      const newpath = `./static/assets/log/${assetName}`
       fs.renameSync(absPath, newpath)
 
       src = src.replace(rex[0], `<video autoplay muted loop src="/assets/log/${assetName}"></video>`)
     })
   }
 
-  var today = moment().subtract(4, 'hours')
+  const today = moment().subtract(4, 'hours')
 
-  var frontmatter = `---
+  const frontmatter = `---
 title: ${title}
 date: ${today.format('YYYY-MM-DD HH:mm:00 +0100')}
 ---
 
 `
-  var dest = frontmatter + src
-  var finalname = `${today.format('YYYY-MM-DD')}-${filename}.md`
+  const dest = frontmatter + src
+  const finalname = `${today.format('YYYY-MM-DD')}-${filename}.md`
   fs.writeFileSync(`./content/log/${finalname}`, dest)
   fs.unlinkSync(`./pub/${f}`)
 })
