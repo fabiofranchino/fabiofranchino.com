d668e8cc9d121d7b40d3bb33395bb515b9e93b85
abusedmedia <hello@fabiofranchino.com>
2021-10-23 15:37:39 +0200

    card

--- pages/blog/_id.vue
+++ pages/blog/_id.vue
@@ -100,7 +100,7 @@ export default {
     // },
     head () {
         const coverImage = this.page.cover ? '&cover=https://www.fabiofranchino.com' + this.page.cover : ''
-        let cover = process.env.NUXT_ENV_SOCIAL_CARD_GENERATOR_URL + '?title=' + this.page.title + '&cta=' + stats.blog + ' posts and counting' + coverImage
+        let cover = process.env.NUXT_ENV_SOCIAL_CARD_GENERATOR_URL + '?title=' + this.page.title + '&cta=' + stats.blog + '&check=article&site=blog' + coverImage
         return {
             title: this.page.title,
             meta: [
