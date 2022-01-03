5a3dc98ed3d447d061dcb147d8616d001b9c5ffd
abusedmedia <hello@fabiofranchino.com>
2022-01-03 09:28:34 +0100

    upd api cloud

--- pages/log/index.vue
+++ pages/log/index.vue
@@ -35,7 +35,7 @@ export default {
         store.commit('setRelated', null)
 
         res.forEach(d => {
-            d.cover = 'https://app.presenta.cc/f/url/zGywhb2oJn:co813Eaci?&title=' + d.title + '&w=450'
+            d.cover = 'https://cloud.presenta.cc/v1/url/zGywhb2oJn:co813Eaci?&title=' + d.title + '&w=450'
         })
 
         return {
