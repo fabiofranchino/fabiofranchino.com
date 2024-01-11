f07c56e9883b4951f95001a13bd6ca80a15bbd32
abusedmedia <hello@fabiofranchino.com>
2024-01-11 16:07:56 +0100

    upd

--- static/sw-inject.js
+++ static/sw-inject.js
@@ -1,3 +1,9 @@
+self.addEventListener('fetch', function(event) {
+    const url = event.request.url
+    const status = event.request.status
+    console.log(url, status)
+})
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register("/sw.js");
- }
+}
