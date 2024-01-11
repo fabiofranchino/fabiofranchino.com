6a32598edd769c8283ce705d6d8e9ad2a7a96896
abusedmedia <hello@fabiofranchino.com>
2024-01-11 16:13:08 +0100

    upd

--- static/sw.js
+++ static/sw.js
@@ -1,6 +1,13 @@
+self.addEventListener('fetch', function(event) {
+    const url = event.request.url
+    const status = event.request.status
+    console.log(url, status)
+})
+
 self.addEventListener("install", event => {
     console.log("Service worker installed")
 })
- self.addEventListener("activate", event => {
+
+self.addEventListener("activate", event => {
     console.log("Service worker activated")
 })
