0cafb086f029f9fb625575d06c68b877ff2f3a2e
abusedmedia <hello@fabiofranchino.com>
2024-08-28 09:47:44 +0200

    banner

--- components/Banners.vue
+++ components/Banners.vue
@@ -5,10 +5,10 @@
             <br /><a href="https://askyourdata.cc/" target="_blank">AskYourData</a>: AI/LLM based data analytics (JS SDK + Tool).
             <br />It's dope! 🤯
         </div>
-        <div class="banner">
+        <!-- <div class="banner">
             <b>New Venture!</b>
             <br /><a href="https://eloquentops.com/" target="_blank">EloquentOps</a>: a <b>Retool</b> bootique for rapid software development.
             <br />We're damn fast! 🚀
-        </div>
+        </div> -->
     </div>
 </template>
